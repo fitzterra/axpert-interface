4ded88d815edd3077b8ab60862f7502358fd1786
Tom Coetser <fitzterra@icave.net>
2024-04-08 19:29:44 +0200

    Add missing paho-mqtt requirement to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="axpert-interface",
     version="0.2.0",
     py_modules=["axpert", "entities"],
-    install_requires=["click>=8.1.7", "crcmod>=1.7", "prettytable>=3.10.0"],
+    install_requires=["click>=8.1.7", "crcmod>=1.7", "prettytable>=3.10.0", "paho-mqtt>=2.0.0"],
     entry_points={
         "console_scripts": [
             "axpert = axpert:cli",
