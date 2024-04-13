6b30817c890cbb265333e65e68054280125b2849
Tom Coetser <fitzterra@icave.net>
2024-04-13 14:15:31 +0200

    Update setup.py to include lib module

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ from setuptools import setup
 setup(
     name="axpert-interface",
     version="0.2.1",
-    py_modules=["axpert", "entities"],
-    install_requires=["click>=8.1.7", "crcmod>=1.7", "prettytable>=3.10.0", "paho-mqtt>=2.0.0"],
+    py_modules=["axpert", "entities", "lib"],
+    install_requires=[
+        "click>=8.1.7",
+        "crcmod>=1.7",
+        "prettytable>=3.10.0",
+        "paho-mqtt>=2.0.0",
+    ],
     entry_points={
         "console_scripts": [
             "axpert = axpert:cli",
